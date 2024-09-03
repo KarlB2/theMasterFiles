75c9e4af202dc1b68533b052db56b3dd75f59c0f
KarlB2 <karlbusse242@gmail.com>
2024-09-03 10:47:31 -0500

    Finished Block07

---
+++ block07/script.js
@@ -0,0 +1,10 @@
+//Tells the user that they have been selected to create a secret vault code
+userMessage = "You have been selected to create a secret passcode"
+
+//The secret Vault Codes
+key1 = 10 - 5;
+key2 = 70 / 5;
+key3 = 25 + 4;
+
+//Alert Code I stole from the internet cause I forgot
+window.alert(`The Code is ${key1} - ${key2} - ${key3}. Don't tell anyone`);
