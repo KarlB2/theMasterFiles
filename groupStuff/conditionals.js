bd220998a367c2611f402e0a73c274d21159820f
KarlB2 <karlbusse242@gmail.com>
2024-09-24 20:08:49 -0500

    block13

---
+++ groupStuff/conditionals.js
@@ -0,0 +1,33 @@
+/*
+1. if raining, stay home.
+
+if (it is raining) {
+    stay home
+}
+
+2. Allow entry only when customer is over 18 years old
+
+if (customer is over 18) {
+    allow entry
+}
+
+3. Dont prompt to join mailing list to existing users
+
+    if user exists, the don't prompt them to join the mailing list
+
+4. Give a free coffee to existing users with more than 5 purchases and to any customer if they have a coupon
+
+5. If a user receives a new message and they have not snoozed notifications, then if they have a custom sound, then player the custom sound, else play the default sound
+
+
+
+
+if (user receives a new message && they have not snoozed notifications) {
+    if (they have a custom sound) {
+        play the custom sound
+    } else {
+        play the default sound 
+    }
+}
+
+*/
