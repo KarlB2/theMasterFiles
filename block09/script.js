3238679d6835fd33a00df11044aa7e8a12d1bd0a
KarlB2 <karlbusse242@gmail.com>
2024-10-03 19:38:40 -0500

    Finished block16

--- block09/script.js
+++
@@ -1,8 +0,0 @@
-const form = document.querySelector('#form')
-const submitButton = document.querySelector('#submit')
-
-form.addEventListener('submit', (e) => {
-    submitButton.disabled = true
-    e.preventDefault()
-    window.location.href = window.location.origin + '/block09/success.html'
-})
