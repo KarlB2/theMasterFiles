eb34de2f67db04e057fbd9b1ebe50df26cbb198f
KarlB2 <karlbusse242@gmail.com>
2024-11-14 19:53:45 -0600

    Finished first part

---
+++ block26/block26-vite/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import ContactList from './components/ContactList.jsx'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <ContactList />
+    </>
+  );
+}
+
+export default App
