f7f27a40c8bb53741091010d146f7937baec1b22
KarlB2 <karlbusse242@gmail.com>
2024-11-21 09:52:30 -0600

    Finished Block28

---
+++ block28/vite-28/src/App.jsx
@@ -0,0 +1,37 @@
+import { Routes, Route, Link } from "react-router-dom";
+import './App.css'
+import Green from "./Green";
+import Red from './Red';
+import Blue from './Blue';
+
+function App() {
+  return (
+    <>
+      <div id="container">
+        <h1>Hello React Router!</h1>
+        <div id="navbar">
+          <Link to="/">Home</Link> |
+          <Link to="/red">Red</Link> |
+          <Link to="/blue">Blue</Link> |
+          <Link to="/green">Green</Link>
+        </div>
+
+        <div id="main-section">
+          <Routes>
+            <Route path="/blue" element={<Blue />} />
+            <Route path="/red" element={<Red />} />
+            <Route path="/green" element={<Green />} />
+          </Routes>
+        </div>
+        <div className="footer navbar">
+          <Link to="/">Home</Link> |
+          <Link to="/red">Red</Link> |
+          <Link to="/blue">Blue</Link> |
+          <Link to="/green">Green</Link>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
